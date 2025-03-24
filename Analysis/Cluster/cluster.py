fcab807ac50b0dd22bc67bfae6f5e3630fce3865
wognsths <2021122006@yonsei.ac.kr>
2025-03-25 02:15:49 +0900

    jhshon

--- Analysis/Cluster/cluster.py
+++ Analysis/Cluster/cluster.py
@@ -1,57 +1,33 @@
 import numpy as np
 from datetime import datetime, timedelta
-
 import hdbscan
 import json
 import os
+from tqdm import tqdm
+import sys
 
-
-# Depreciated
 def get_reference(ref_N: int, file_dir: str):
     with open(f"{file_dir}/reference_{ref_N}.json") as f:
         reference = json.load(f)
-
-    # tmp = None
-    # ref_dict = {}
-    # for row in reference:
-    #     key = list(row.keys())[0]
-    #     file_value = row[key][-1]
-
-    #     if tmp is not None and file_value != tmp:
-    #         date_obj = datetime.strptime(key, "%Y%m%d") - timedelta(days=1)
-    #         # ref_dict[date_obj] = tmp
-    #         ref_dict[date_obj.strftime("%Y%m%d")] = tmp
-
-    #     tmp = file_value
     date_list = [list(row.keys())[0] for row in reference]
-
     return date_list
 
-def HDBSCAN(
-        embeddings,
-        metric: str = 'cosine',
-        cluster_selection_method: str = "eom",
-        min_cluster_size = None
-    ):
+def HDBSCAN(embeddings, metric: str = 'cosine', cluster_selection_method: str = "eom", min_cluster_size = None):
     if not min_cluster_size:
         min_cluster_size = embeddings.shape[0] // 10
-    clusterer = hdbscan.HDBSCAN(
-        min_cluster_size=min_cluster_size,
-        metric=metric,
-        cluster_selection_method=cluster_selection_method
-    )
-
-    clusterer.fit(embeddings)
-    labels = clusterer.labels_
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size,
+                                metric=metric,
+                                cluster_selection_method=cluster_selection_method)
+    try:
+        clusterer.fit(embeddings)
+        labels = clusterer.labels_
+    except:
+        labels = None
 
     return labels
 
 class HDBSCAN_cluster:
-    def __init__(
-            self,
-            embedding_path: str,
-            reference_path: str
-        ):
+    def __init__(self, embedding_path: str, reference_path: str):
         with open(reference_path, mode="r") as f:
             self.reference = json.load(f)
         self.embedding_path = embedding_path
@@ -59,53 +35,52 @@ class HDBSCAN_cluster:
     def get_date_list(self):
         date_list = [list(row.keys())[0] for row in self.reference]
         self.date_list = date_list
-    
+
     def get_embeddingfile(self):
-        self.embeddinglist = []
+        unique_embeddings = {}
         file_list = [os.path.join(self.embedding_path, file) for file in os.listdir(self.embedding_path) if file.endswith(".json")]
         for file in file_list:
-            with open(file, mode = "r") as f:
+            with open(file, mode="r") as f:
                 jsonfile = json.load(f)
-            self.embeddinglist.extend(jsonfile)
+            for item in jsonfile:
+                if item["ID"] not in unique_embeddings:
+                    unique_embeddings[item["ID"]] = item
+        self.embeddinglist = list(unique_embeddings.values())
 
     def process_HDBSCAN(
             self,
             date: str,
-            embedding_arr:list,
-            ID_arr:list,
-            link_arr:list,
+            embedding_arr: list,
+            ID_arr: list,
+            link_arr: list,
             metric: str = 'cosine',
             cluster_selection_method: str = "eom",
             min_cluster_size = None
         ):
         labels = HDBSCAN(
             embedding_arr,
-            metric = metric,
+            metric=metric,
             cluster_selection_method=cluster_selection_method,
-            min_cluster_size=min_cluster_size
-        )
-        Info_dict = {
-            "Date": date,
-            "Center_Link": []
-        }
+            min_cluster_size=min_cluster_size)
+        if labels is None:
+            return None
+        clusters = {}
+        noise = []
         for index, label in enumerate(labels):
             if label != -1:
-                if f"Cluster_{label}" not in Info_dict.keys():
-                    Info_dict[f"Cluster_{label}"] = [ID_arr[index]]
-                else:
-                    Info_dict[f"Cluster_{label}"].append(ID_arr[index])
+                clusters.setdefault(label, []).append(ID_arr[index])
             else:
-                if "Noise" not in Info_dict.keys():
-                    Info_dict["Noise"] = [ID_arr[index]]
-                else:
-                    Info_dict["Noise"].append(ID_arr[index])
-
+                noise.append(ID_arr[index])
+        Info_dict = {"Date": date, "Center_Link": []}
+        for cluster_label in sorted(clusters.keys()):
+            Info_dict[f"Cluster_{cluster_label}"] = clusters[cluster_label]
+        if noise:
+            Info_dict["Noise"] = noise
         unique_label = sorted(list(set(labels)))
         if -1 in unique_label:
             u_label = unique_label[1:]
         else:
             u_label = unique_label
-
         for entity in u_label:
             ll = []
             link_list = []
@@ -113,22 +88,23 @@ class HDBSCAN_cluster:
                 if entity == label:
                     ll.append(embedding_arr[index])
                     link_list.append(link_arr[index])
-            
-            mean_vector = np.mean(np.array(ll), axis = 0)
-            dist = np.linalg.norm(np.array(ll) - mean_vector, axis = 1)
-
+            mean_vector = np.mean(np.array(ll), axis=0)
+            dist = np.linalg.norm(np.array(ll) - mean_vector, axis=1)
             Info_dict["Center_Link"].append(link_list[np.argsort(dist)[0]])
-
         return Info_dict
 
     def process_by_date(
             self,
             metric: str = 'cosine',
             cluster_selection_method: str = "eom",
-            min_cluster_size = None
+            min_cluster_size = None,
+            output_path: str = None
         ):
         INFO_list = []
-        for index, date in enumerate(self.date_list):
+        if output_path is None:
+            output_path = "./Data/News/Clusters/cluster_result.json"
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        for index, date in tqdm(enumerate(self.date_list), total=len(self.date_list)):
             IDs = self.reference[index]["IDs"]
             embedding_arr = []
             ID_arr = []
@@ -138,43 +114,33 @@ class HDBSCAN_cluster:
                     embedding_arr.append(entity["Embedding"])
                     link_arr.append(entity["Link"])
                     ID_arr.append(entity["ID"])
-
             embedding_arr = np.array(embedding_arr)
             link_arr = np.array(link_arr)
             ID_arr = np.array(ID_arr)
-
-            Info_dict = self.process_HDBSCAN(
-                date=date,
-                embedding_arr=embedding_arr,
-                ID_arr=ID_arr,
-                link_arr=link_arr,
-                metric=metric,
-                cluster_selection_method=cluster_selection_method,
-                min_cluster_size=min_cluster_size
-            )
+            Info_dict = self.process_HDBSCAN(date=date,
+                                             embedding_arr=embedding_arr,
+                                             ID_arr=ID_arr,
+                                             link_arr=link_arr,
+                                             metric=metric,
+                                             cluster_selection_method=cluster_selection_method,
+                                             min_cluster_size=min_cluster_size)
+            if Info_dict is None:
+                continue
             INFO_list.append(Info_dict)
+            if (index + 1) % 100 == 0:
+                with open(output_path, "w", encoding="utf-8") as f:
+                    json.dump(INFO_list, f, indent=4)
+        with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(INFO_list, f, indent=4)
 
-        return INFO_list
-
-
-def run_HDBSCAN(
-        embedding_path: str,
-        reference_path: str,
-        metric: str = "cosine",
-        cluster_selection_method: str = "eom",
-        min_cluster_size: int = None
-    ):
-    timeseries_clusterer=HDBSCAN_cluster(
-        embedding_path=embedding_path,
-        reference_path=reference_path
-    )
+def run_HDBSCAN(embedding_path: str,
+                reference_path: str,
+                metric: str = "cosine",
+                cluster_selection_method: str = "eom",
+                min_cluster_size: int = None,
+                output_path: str = None):
+    
+    timeseries_clusterer = HDBSCAN_cluster(embedding_path=embedding_path, reference_path=reference_path)
     timeseries_clusterer.get_date_list()
     timeseries_clusterer.get_embeddingfile()
-
-    INFO_list = timeseries_clusterer.process_by_date(
-        metric=metric,
-        cluster_selection_method=cluster_selection_method,
-        min_cluster_size=min_cluster_size
-    )
-
-    return INFO_list
+    timeseries_clusterer.process_by_date(metric=metric, cluster_selection_method=cluster_selection_method, min_cluster_size=min_cluster_size, output_path=output_path)
--- Analysis/Cluster/main.py
+++ Analysis/Cluster/main.py
@@ -1,10 +1,42 @@
 from argparse import ArgumentParser
 from Analysis.Cluster.cluster import *
 
+import warnings
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 def create_parser() -> ArgumentParser:
     parser = ArgumentParser()
-    parser.add_argument('-r', '--reference_index', type=int, required=True)
-    parser.add_argument('-d', '--embedding_directory', type=str, required=True)
-    parser.add_argument('-m', '--metric', choices=['cosine', 'euclidean'], required=True)
-    parser.add_argument('e', '--')
+    parser.add_argument('-r', '--reference_directory', type=int, required=False)
+    parser.add_argument('-d', '--embedding_directory', type=str, required=False)
+    parser.add_argument('-m', '--metric', choices=['cosine', 'euclidean'], required=False, default='euclidean')
+    parser.add_argument('-c', '--cluster_selection_method', choices=["eom", "leaf"], required=False, default="eom")
+    parser.add_argument('-s', '--min_cluster_size', type=int, required=False)
+
     return parser
+
+def main():
+    args = create_parser().parse_args()
+
+    reference_dir = args.reference_directory
+    if not reference_dir:
+        reference_dir = "./Analysis/Cluster/reference_1.json"
+    embedding_dir = args.embedding_directory
+    if not embedding_dir:
+        embedding_dir = "./Data/News/Embeddings"
+
+    metric = args.metric
+    cluster_selection_method = args.cluster_selection_method
+    min_cluster_size = args.min_cluster_size
+
+    run_HDBSCAN(
+        embedding_path=embedding_dir,
+        reference_path=reference_dir,
+        metric=metric,
+        cluster_selection_method=cluster_selection_method,
+        min_cluster_size=min_cluster_size
+    )
+
+if __name__ == "__main__":
+    main()
